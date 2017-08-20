0405de77736c70b044a65193a2601c685d8314f4
Troels Henriksen <athas@sigkill.dk>
2017-08-20 18:12:26 +0200

    More work.

--- experiments/tools/plot.py
+++ experiments/tools/plot.py
@@ -10,8 +10,6 @@ matplotlib.use('Agg') # For headless use
 import matplotlib.pyplot as plt
 import os
 
-outputfile = sys.argv[1]
-
 rodinia_programs = [("Backprop", "backprop"),
                     ("CFD", "cfd"),
                     ("HotSpot", "hotspot"),
@@ -20,17 +18,26 @@ rodinia_programs = [("Backprop", "backprop"),
                     ("Myocyte", "myocyte"),
                     ("NN", "nn"),
                     ("Pathfinder", "pathfinder"),
-                    ("SRAD", "srad")]
+                    ("SRAD", "srad"),
+                    ("LUD", "lud")]
 
 accelerate_programs = [("Crystal", "crystal"),
                        ("Fluid", "fluid"),
                        ("Mandelbrot", "mandelbrot"),
-                       ("N-body", "nbody")]
+                       ("N-body", "nbody"),
+                       ("Tunnel", "tunnel")]
 
-finpar_programs = [("LocVolCalib", "LocVolCalib_large"),
-                   ("OptionPricing", "OptionPricing_large")]
+finpar_programs = [("LocVolCalib-small", "LocVolCalib_small"),
+                   ("LocVolCalib-medium", "LocVolCalib_medium"),
+                   ("LocVolCalib-large", "LocVolCalib_large"),
+                   ("OptionPricing-small", "OptionPricing_small"),
+                   ("OptionPricing-medium", "OptionPricing_medium"),
+                   ("OptionPricing-large", "OptionPricing_large")]
 
-parboil_programs = [("MRI-Q", "mri-q")]
+parboil_programs = [("MRI-Q", "mri-q"),
+                    ("SGEMM", "sgemm"),
+                    ("Stencil", "stencil"),
+                    ("TPACF", "tpacf")]
 
 def plotting_info(x):
     name, filename = x
@@ -45,86 +52,100 @@ def plotting_info(x):
             except:
                 aux_speedup = 0.0
             return (name, speedup, aux_speedup)
+        
     except:
         print('Skipping %s as %s could not be opened' % (name,speedup_file))
 
-to_plot = list(filter(lambda x: x is not None,
-                      map(plotting_info,
-                          rodinia_programs + finpar_programs + parboil_programs + accelerate_programs)))
+def plot_suite(outputfile, programs, no_aux=False):
+   to_plot = list(filter(lambda x: x is not None, map(plotting_info,programs)))
 
-program_names = [ name for (name, _, _) in to_plot ]
-program_speedups = [ speedup for (_, speedup, _) in to_plot ]
-program_aux_speedups = [ aux_speedup for (_, _, aux_speedup) in to_plot ]
+   program_names = [ name for (name, _, _) in to_plot ]
+   program_speedups = [ speedup for (_, speedup, _) in to_plot ]
+   program_aux_speedups = [ aux_speedup for (_, _, aux_speedup) in to_plot ]
 
-N = len(to_plot)
+   N = len(to_plot)
 
-# the widths of the bar
-width = 0.5
-def compute_space(x):
-    if x[2] == 0.0:
-        return width*2
-    else:
-        return width*3
-spaces = np.array(list(map(compute_space, to_plot)))
+   # the widths of the bar
+   width = 0.5
+   def compute_space(x):
+       if no_aux:
+           return width*2
+       else:
+           return width*3
+   spaces = np.array(list(map(compute_space, to_plot)))
 
-# the x locations for the bars
-ind = width+np.concatenate(([0], np.cumsum(spaces)[:-1]))
+   # the x locations for the bars
+   ind = width+np.concatenate(([0], np.cumsum(spaces)[:-1]))
 
-fig, ax = plt.subplots()
+   fig, ax = plt.subplots()
 
-font = {'family': 'normal',
-        'size' : 9}
-plt.rc('font', **font)
-grey='#aaaaaa'
+#   matplotlib.rcParams['pdf.fonttype'] = 42
+#   matplotlib.rcParams['ps.fonttype'] = 42
+   font = {'family': 'normal',
+           'size' : 10}
+   plt.rc('font', **font)
+   grey='#aaaaaa'
 
-ax.set_ylim([0.0,6.0])
-ax.set_xlim([0.0,ind[-1]+1])
-ax.set_ylabel('Speedup')
-ax.set_xticks(ind+width)
-ax.set_xticklabels(program_names)
-ax.xaxis.set_ticks_position('none')
-ax.yaxis.set_ticks_position('none')
-ax.yaxis.grid(color=grey,zorder=0)
-ax.spines['bottom'].set_color(grey)
-ax.spines['top'].set_color('none')
-ax.spines['left'].set_color(grey)
-ax.spines['right'].set_color('none')
+   ymax=6
 
-gridlines = ax.get_xgridlines() + ax.get_ygridlines()
-for line in gridlines:
-    line.set_linestyle('-')
+   ax.set_ylim([0.0,ymax])
+   ax.set_xlim([0.0,ind[-1]+1])
+   ax.set_ylabel('Speedup')
+   if no_aux:
+       ax.set_xticks(ind)
+   else:
+       ax.set_xticks(ind+width)
+   ax.set_xticklabels(program_names)
+   ax.set_yticks(np.arange(ymax+1))
+   ax.xaxis.set_ticks_position('none')
+   ax.yaxis.set_ticks_position('none')
+   ax.yaxis.grid(color=grey,zorder=0)
+   ax.spines['bottom'].set_color(grey)
+   ax.spines['top'].set_color('none')
+   ax.spines['left'].set_color(grey)
+   ax.spines['right'].set_color('none')
 
-stagger = 0
-for t in ax.get_xticklabels( ):
-    t.set_y( stagger )
-    if stagger == 0:
-        stagger = -0.13
-    else:
-        stagger = 0
+   gridlines = ax.get_xgridlines() + ax.get_ygridlines()
+   for line in gridlines:
+       line.set_linestyle('-')
 
-rects = ax.bar(ind, program_speedups, width, color='#2255ee', zorder=3, edgecolor='white', label='GTX 780')
-aux_rects = ax.bar(ind+width, program_aux_speedups, width, color='#ee5500', zorder=3, edgecolor='white', label='W8100')
+   stagger = 0
+   for t in ax.get_xticklabels( ):
+       t.set_y( stagger )
+       if stagger == 0:
+           stagger = -0.13
+       else:
+           stagger = 0
 
-def label_rect(rect):
-    height = rect.get_height()
+   rects = ax.bar(ind, program_speedups, width, color='#2255ee', zorder=3, edgecolor='white', label='NVIDIA K40')
+   if no_aux:
+       aux_rects = []
+   else:
+       aux_rects = ax.bar(ind+width, program_aux_speedups, width, color='#ee5500', zorder=3, edgecolor='white', label='AMD W8100')
 
-    if height == 0.0:
-        return
+   def label_rect(rect):
+       height = rect.get_height()
 
-    font = {'family': 'sans-serif',
-            'weight': 'bold',
-            'size': 8,
-    }
-    bounded_height = min(6.0, height)
-    ax.text(rect.get_x() + rect.get_width(), 1.05*bounded_height,
-            '%.2f' % height,
-            ha='center', va='bottom', fontdict=font, rotation=45)
+       if height == 0.0:
+           return
 
-if os.getenv('FUTHARK_LEGENDS'):
-    map(label_rect, rects)
-    map(label_rect, aux_rects)
-    ax.legend(bbox_to_anchor=(0., 1.15, 1., .115), loc=3, ncol=2, borderaxespad=0.)
+       font = {'family': 'sans-serif',
+               'size': 10,
+       }
+       bounded_height = min(ymax, height)
+       ax.text(rect.get_x() + rect.get_width(), 1.05*bounded_height,
+               '%.2f' % height,
+               ha='center', va='bottom', fontdict=font, rotation=45)
 
-fig.set_size_inches(8.5, 1.5)
-plt.rc('text')
-plt.savefig(outputfile, bbox_inches='tight')
+   map(label_rect, rects)
+   map(label_rect, aux_rects)
+   ax.legend(bbox_to_anchor=(0., 1.5, 1., .115), loc=1, ncol=2, borderaxespad=0.)
+
+   fig.set_size_inches(len(programs), 1.5)
+   plt.rc('text')
+   plt.savefig(outputfile, bbox_inches='tight')
+
+plot_suite('rodinia.pdf', rodinia_programs)
+plot_suite('accelerate.pdf', accelerate_programs, True)
+plot_suite('finpar.pdf', finpar_programs)
+plot_suite('parboil.pdf', parboil_programs)
--- experiments/tools/table.py
+++ experiments/tools/table.py
@@ -18,17 +18,28 @@ rodinia_programs = [("Backprop", "backprop"),
                     ("Myocyte", "myocyte"),
                     ("NN", "nn"),
                     ("Pathfinder", "pathfinder"),
-                    ("SRAD", "srad")]
+                    ("SRAD", "srad"),
+                    ("LUD", "lud")]
 
 accelerate_programs = [("Crystal", "crystal"),
                        ("Fluid", "fluid"),
                        ("Mandelbrot", "mandelbrot"),
-                       ("N-body", "nbody")]
+                       ("N-body", "nbody"),
+                       ("Tunnel", "tunnel")]
 
-finpar_programs = [("LocVolCalib", "LocVolCalib_large"),
-                   ("OptionPricing", "OptionPricing_large")]
+finpar_programs = [("LocVolCalib-small", "LocVolCalib_small"),
+                   ("LocVolCalib-medium", "LocVolCalib_medium"),
+                   ("LocVolCalib-large", "LocVolCalib_large"),
+                   ("OptionPricing-small", "OptionPricing_small"),
+                   ("OptionPricing-medium", "OptionPricing_medium"),
+                   ("OptionPricing-large", "OptionPricing_large")]
 
-parboil_programs = [("MRI-Q", "mri-q")]
+parboil_programs = [("MRI-Q", "mri-q"),
+                    ("SGEMM", "sgemm"),
+                    ("Stencil", "stencil"),
+                    ("TPACF", "tpacf")]
+
+outdir=sys.argv[1]
 
 def ref_filename_closure(ref):
     return lambda d, filename: os.path.join(d, filename + '-' + ref + '.avgtime')
@@ -48,6 +59,15 @@ def get_avgtimes(d, ref_filename, filename):
     return (get_ref_avgtime(d, ref_filename, filename),
             get_fut_avgtime(d, ref_filename, filename))
 
+def put(filename, ref, fut, auxref, auxfut):
+    with open(outdir + '/' + filename + '-ref.tex', 'w') as f:
+        f.write(ref)
+    with open(outdir + '/' + filename + '-futhark.tex', 'w') as f:
+        f.write(fut)
+    with open(outdir + '/' + filename + '-auxref.tex', 'w') as f:
+        f.write(auxref)
+    with open(outdir + '/' + filename + '-auxfuthark.tex', 'w') as f:
+        f.write(auxfut)
 
 def table_info_closure(ref_filename):
     def table_info(x):
@@ -58,30 +78,18 @@ def table_info_closure(ref_filename):
                 aux_fut_avgtime = get_fut_avgtime('aux_runtimes', ref_filename, filename)
                 try:
                     aux_ref_avgtime = get_ref_avgtime('aux_runtimes', ref_filename, filename)
-                    return (name, ref_avgtime, fut_avgtime, aux_ref_avgtime, aux_fut_avgtime)
+                    put(filename, ref_avgtime, fut_avgtime, aux_ref_avgtime, aux_fut_avgtime)
                 except:
-                    return (name, ref_avgtime, fut_avgtime, '---', aux_fut_avgtime)
+                    put(filename, ref_avgtime, fut_avgtime, '---', aux_fut_avgtime)
             except:
-                return (name, ref_avgtime, fut_avgtime, '---', '---')
-        except:
+                put(filename, ref_avgtime, fut_avgtime, '---', '---')
+        except Exception as e:
+            print(e)
             sys.stderr.write('Skipping %s as the file could not be opened.\n' % name)
             return None
     return table_info
 
-to_table = list(filter(lambda x: x is not None,
-                       map(table_info_closure(ref_filename_closure('rodinia')), rodinia_programs) +
-                       map(table_info_closure(ref_filename_closure('finpar')), finpar_programs) +
-                       map(table_info_closure(ref_filename_closure('parboil')), parboil_programs) +
-                       map(table_info_closure(ref_filename_closure('accelerate')), accelerate_programs)))
-
-print(r'''\begin{tabular}{lrrrr}''')
-
-if os.getenv('FUTHARK_LEGENDS'):
-    print(r'''& \multicolumn{2}{c}{\textbf{NVIDIA GTX780}} & \multicolumn{2}{c}{\textbf{AMD W8100}} \\''')
-
-print(r'''\textbf{Benchmark} & \textbf{Ref.} & \textbf{Futhark} & \textbf{Ref.} & \textbf{Futhark}\\''')
-
-for row in to_table:
-    print(r'''%s & %s & %s & %s & %s \\''' % row)
-
-print(r'''\end{tabular}''')
+map(table_info_closure(ref_filename_closure('rodinia')), rodinia_programs)
+map(table_info_closure(ref_filename_closure('finpar')), finpar_programs)
+map(table_info_closure(ref_filename_closure('parboil')), parboil_programs)
+map(table_info_closure(ref_filename_closure('accelerate')), accelerate_programs)
